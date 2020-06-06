64052e958d0882ed1b59235e8546c86d4be9a981
Yinaê <yinae04@gmail.com>
2020-06-06 14:33:06 -0300

    Adicionando arquivos

---
+++ age.js
@@ -0,0 +1,5 @@
+var age = parseInt(prompt("Qual a sua idade ?"))
+
+var agInSeconds = age * 365 * 24 * 60 * 60
+
+document.write("Em seus " + age + " anos de idade, se passaram " + agInSeconds + " segundos")
